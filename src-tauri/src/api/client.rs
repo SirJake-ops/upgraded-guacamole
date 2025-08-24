4ff461acebe77dd6e117bc8abbe1b1d11d99a860
Jacob Pagan <jacob.pagan10@gmail.com>
2025-08-24 14:12:48 -0500

    Started to write the REST client

---
+++ src-tauri/src/api/client.rs
@@ -0,0 +1,13 @@
+pub struct ApiClient {
+    pub base_url: String,
+    pub api_key: String,
+}
+
+impl ApiClient {
+    pub fn new(base_url: &str, api_key: &str) -> Self {
+        Self {
+            base_url: base_url.to_string(),
+            api_key: api_key.to_string(),
+        }
+    }
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
 mod models;
+mod api;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use api::client::ApiClient;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,7 +11,9 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let api_client = ApiClient::new("http://localhost:5000", "your_secret_key");
     tauri::Builder::default()
+        .manage(api_client)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())

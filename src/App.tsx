09d95409e29e1fef8ac76eb936de673f83a9a171
Jake Pagan <jacob.pagan10@gmail.com>
2026-03-16 10:11:36 -0500

    Started fleshing out a login form for the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import "./App.css";
-import {invoke} from "@tauri-apps/api/core";
+import LoginPage from "./components/auth/LoginPage";
 
 function App() {
-
-    async function hello() {
-        const message = await invoke('test_hello');
-        alert(message);
-    }
-
-  return (
-    <main class="container">
-      <h1>Welcome to Tauri + Solid</h1>
-        <div>
-            <button onClick={() => hello()}>Hello</button>
-        </div>
-
-    </main>
-  );
+  return <LoginPage />;
 }
 
 export default App;
